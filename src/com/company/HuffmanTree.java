d643147383919c23e04b5956ebdd7f369beb7058
TheUnknownLegend1 <riyaanbakhda2@gmail.com>
2017-06-08 17:08:47 +0530

    First commit

---
+++ src/com/company/HuffmanTree.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Hashtable;
+public class HuffmanTree {
+    String target;
+    public HuffmanTree(String t){
+
+    }
+
+}
---
+++ src/com/company/sortedList.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class sortedList{
+    class Node{
+        Node next;
+        int frequency;
+        char c;
+        public Node(int f, char ch){
+            frequency = f;
+            c = ch;
+        }
+    }
+    Node first;
+    public void add(int frequency, char c){
+
+    }
+}
